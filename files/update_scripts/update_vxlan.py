3d1db735688b8183c6813b7d3de45fabfa3eb20d
Gagan Deep <pandafy.dev@gmail.com>
2021-04-20 19:40:03 +0530

    [feature] Added ansible role for managing WireGuard/VXLAN server
    The role installs WireGuard and NetworkManager for managing VPN configuration.
    It setups a flask app with SSL using uWSGI for pulling VPN server
    configuratiion from OpenWISP.
    
    - Added support for
      - CentOS 7 & 8
      - Ubuntu 18.04 & 20.04
      - Debian 10
    - Added automated tests
    - Added documentation

---
+++ files/update_scripts/update_vxlan.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python3
+
+import json
+import subprocess
+import sys
+
+try:
+    peer_file_path = sys.argv[1]
+except IndexError:
+    print('peer file must be passed as firs argument', file=sys.stderr)
+    sys.exit(1)
+
+try:
+    with open(peer_file_path, 'r') as peer_file:
+        contents = peer_file.read()
+except FileNotFoundError as e:
+    print(e, file=sys.stderr)
+    sys.exit(2)
+
+try:
+    peers = json.loads(contents)
+    assert isinstance(peers, list)
+except Exception as e:
+    print(f'Error while parsing JSON file: {e}', file=sys.stderr)
+    sys.exit(3)
+
+
+remote_peers = {}
+
+for peer in peers:
+    remote_peers[f'vxlan-vxlan{peer["vni"]}'] = peer
+
+
+class Nmcli:
+    @classmethod
+    def _exec_command(cls, command):
+        process = subprocess.Popen(
+            command.split(' '), stdout=subprocess.PIPE, stderr=subprocess.PIPE
+        )
+        stdout, stderr = process.communicate()
+        if stderr:
+            raise ValueError(stderr)
+        return stdout.decode('utf8').strip()
+
+    @classmethod
+    def list_connections(cls, type=None):
+        output = cls._exec_command('nmcli connection show')
+        lines = output.split('\n')
+        connections = []
+        for line in lines[1:]:
+            parts = line.split()
+            connection = {
+                'name': parts[0].strip(),
+                'uuid': parts[1].strip(),
+                'type': parts[2].strip(),
+                'device': parts[3].strip(),
+            }
+            if not type or type and type == connection['type']:
+                connections.append(connection)
+        return connections
+
+    @classmethod
+    def get_connection(cls, connection):
+        output = cls._exec_command(f'nmcli connection show {connection}')
+        data = {}
+        lines = output.split('\n')
+        for line in lines:
+            parts = line.split()
+            data[parts[0][:-1]] = parts[1]
+        return data
+
+    @classmethod
+    def get_local_vxlan_peers(cls):
+        peers = {}
+        vxlan_connections = cls.list_connections(type='vxlan')
+        for vxlan in vxlan_connections:
+            data = cls.get_connection(vxlan['uuid'])
+            peers[data['connection.id']] = {
+                'remote': data['vxlan.remote'],
+                'vni': int(data['vxlan.id']),
+            }
+        return peers
+
+    @classmethod
+    def add_connection(cls, ifname, vni, remote):
+        return cls._exec_command(
+            f'nmcli connection add type vxlan ifname {ifname} '
+            f'id {vni} remote {remote} destination-port 4789 '
+            'ipv4.method link-local ipv6.method link-local'
+        )
+
+    @classmethod
+    def edit_connection(cls, connection, vni, remote):
+        return cls._exec_command(
+            f'nmcli connection modify {connection} vxlan.id {vni} vxlan.remote {remote}'
+        )
+
+    @classmethod
+    def delete_connection(cls, connection):
+        return cls._exec_command(f'nmcli connection delete {connection}')
+
+
+local_peers = Nmcli.get_local_vxlan_peers()
+
+
+for connection_name, peer_data in local_peers.items():
+    if connection_name not in remote_peers:
+        Nmcli.delete_connection(connection_name)
+        print(f'Removed {connection_name}')
+
+
+for connection_name, peer_data in remote_peers.items():
+    vni = peer_data['vni']
+    remote = peer_data['remote']
+    if connection_name not in local_peers:
+        Nmcli.add_connection(f'vxlan{vni}', vni, remote)
+        print(f'Added {connection_name}')
+        continue
+    elif peer_data == local_peers[connection_name]:
+        print(f'Skipping {connection_name}, already up to date')
+        continue
+    else:
+        Nmcli.edit_connection(connection_name, vni, remote)
+        print(f'Updated {connection_name}')
---
+++ templates/flask/vpn_updater.py
@@ -0,0 +1,39 @@
+import subprocess
+
+from flask import Flask, Response, request
+
+app = Flask(__name__)
+
+KEY = '{{ openwisp2_wireguard_flask_key }}'
+UPDATER_SCRIPTS = [
+    '{{ openwisp2_wireguard_directory }}/update_wireguard.sh check_config',
+]
+
+
+def _exec_command(command):
+    process = subprocess.Popen(
+        command.split(' '),
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        close_fds=True,
+    )
+    stdout, stderr = process.communicate()
+    exit_code = process.wait(timeout=10)
+    if exit_code != 0:
+        raise subprocess.SubprocessError()
+
+
+@app.route('{{ openwisp2_wireguard_flask_endpoint }}', methods=['POST'])
+def update_vpn_config():
+    if request.args.get('key') != KEY:
+        return Response(status=403)
+    for script in UPDATER_SCRIPTS:
+        try:
+            _exec_command(script)
+        except subprocess.SubprocessError:
+            return Response(status=500)
+    return Response(status=200)
+
+
+if __name__ == '__main__':
+    app.run()
