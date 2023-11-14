1a2d3bc51ef798710ff6f211986951e098ca010d
clutch2sft <clutch2sft@trip-g.com>
2023-11-14 06:25:34 -0500

    Security updates including logging, x-site scripting checks, api key verification improvements

--- templates/flask/vpn_updater.py
+++ templates/flask/vpn_updater.py
@@ -1,4 +1,8 @@
 import subprocess
+import logging
+from logging.handlers import RotatingFileHandler
+from werkzeug.security import safe_str_cmp
+
 
 from flask import Flask, Response, request
 
@@ -8,7 +12,16 @@ KEY = '{{ openwisp2_wireguard_flask_key }}'
 UPDATER_SCRIPTS = [
     '{{ openwisp2_wireguard_path }}/update_wireguard.sh check_config',
 ]
+ALLOWED_SCRIPTS = ["update_wireguard.sh check_config"]
 
+
+file_handler = RotatingFileHandler('{{ openwisp2_wireguard_path }}/log/openwisp-vpnupdater.log', maxBytes=104857600, backupCount=10)
+file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+file_handler.setLevel(logging.INFO)
+app.logger.addHandler(file_handler)
+
+def is_script_allowed(script):
+    return script in ALLOWED_SCRIPTS
 
 def _exec_command(command):
     process = subprocess.Popen(
@@ -20,19 +33,68 @@ def _exec_command(command):
     stdout, stderr = process.communicate()
     exit_code = process.wait(timeout=10)
     if exit_code != 0:
+        app.logger.error("Script execution failed: %s, Error: %s", command, stderr.decode('utf-8'))
         raise subprocess.SubprocessError()
 
 
 @app.route('{{ openwisp2_wireguard_flask_endpoint }}', methods=['POST'])
 def update_vpn_config():
-    if request.args.get('key') != KEY:
+    app.logger.info("Received request to update VPN config")
+    #changed this so we can do input validation and prevent timing hacks
+    provided_key = request.args.get('key')
+    if not provided_key or not safe_str_cmp(provided_key, KEY):
+        #make sure we log attempts to get through with an invalid key.
+        client_info = {
+            'ip_address': request.remote_addr,
+            'user_agent': request.user_agent.string,
+            'requested_url': request.url,
+            'http_method': request.method
+        }
+        app.logger.warning("Authentication failed - invalid or missing key provided. Client info: %s", client_info)
         return Response(status=403)
     for script in UPDATER_SCRIPTS:
         try:
+            #make sure the called scripted is what we expect it to be and if it isn't then exit unauthorized
+            if not is_script_allowed(script):
+                app.logger.error("Unauthorized script access attempt: %s", script)
+                return Response(status=403)
             _exec_command(script)
+            app.logger.info("Script executed successfully: %s", script)
         except subprocess.SubprocessError:
+            app.logger.error("Script execution failed: %s", script)
             return Response(status=500)
     return Response(status=200)
+
+@app.errorhandler(500)
+def handle_500_error(exception):
+    app.logger.error("An internal error occurred: %s", str(exception))
+    return Response(status=500)
+from flask import Flask, Response
+
+@app.after_request
+def set_csp(response):
+    response.headers["Content-Security-Policy"] = "default-src 'self'"
+    return response
+@app.after_request
+def set_x_frame_options(response):
+    response.headers["X-Frame-Options"] = "SAMEORIGIN"
+    return response
+@app.after_request
+def set_x_content_type_options(response):
+    response.headers["X-Content-Type-Options"] = "nosniff"
+    return response
+@app.after_request
+def set_hsts(response):
+    response.headers["Strict-Transport-Security"] = "max-age=31536000; includeSubDomains"
+    return response
+@app.after_request
+def set_x_xss_protection(response):
+    response.headers["X-XSS-Protection"] = "1; mode=block"
+    return response
+@app.after_request
+def set_referrer_policy(response):
+    response.headers["Referrer-Policy"] = "strict-origin-when-cross-origin"
+    return response
 
 
 if __name__ == '__main__':
