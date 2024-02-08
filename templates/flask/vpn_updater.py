fd8877be3e8476192285f9a9ee8990283eb02ccb
clutch2sft <148905f4@opayq.com>
2024-02-08 18:43:11 +0530

    [feature] Added logging to the Flask app
    Co-authored-by: Gagan Deep <pandafy.dev@gmail.com>

--- templates/flask/vpn_updater.py
+++ templates/flask/vpn_updater.py
@@ -1,3 +1,5 @@
+import hmac
+import logging
 import subprocess
 
 from flask import Flask, Response, request
@@ -8,6 +10,12 @@ KEY = '{{ openwisp2_wireguard_flask_key }}'
 UPDATER_SCRIPTS = [
     '{{ openwisp2_wireguard_path }}/update_wireguard.sh check_config',
 ]
+
+
+# Configure logging
+app.logger.setLevel(
+    getattr(logging, '{{ openwisp2_wireguard_logging_level }}', 'WARNING')
+)
 
 
 def _exec_command(command):
@@ -23,16 +31,58 @@ def _exec_command(command):
         raise subprocess.SubprocessError()
 
 
+def _log(level, message, request):
+    client_info = {
+        'ip_address': request.remote_addr,
+        'user_agent': request.user_agent.string,
+        'requested_url': request.url,
+        'http_method': request.method,
+    }
+    getattr(app.logger, level)(f'{message} Client info: {client_info}')
+
+
 @app.route('{{ openwisp2_wireguard_flask_endpoint }}', methods=['POST'])
 def update_vpn_config():
-    if request.args.get('key') != KEY:
+    _log('info', 'Received request to update VPN config', request)
+    request_key = request.args.get('key')
+    if not request_key or not hmac.compare_digest(request_key, KEY):
+        _log(
+            'warning',
+            'Authentication failed - invalid or missing key provided.',
+            request,
+        )
         return Response(status=403)
     for script in UPDATER_SCRIPTS:
         try:
             _exec_command(script)
         except subprocess.SubprocessError:
+            _log('error', f'Failed to execute script: "{script}"', request)
             return Response(status=500)
+        else:
+            _log('info', 'Script executed successfully', request)
     return Response(status=200)
+
+
+@app.errorhandler(500)
+def handle_500_error(exception):
+    app.logger.error("An internal error occurred: %s", str(exception))
+    return Response(status=500)
+
+
+@app.after_request
+def set_security_headers(response):
+    security_headers = {
+        "Content-Security-Policy": "default-src 'self'",
+        "X-Frame-Options": "SAMEORIGIN",
+        "X-Content-Type-Options": "nosniff",
+        "Strict-Transport-Security": "max-age=31536000; includeSubDomains",
+        "X-XSS-Protection": "1; mode=block",
+        "Referrer-Policy": "strict-origin-when-cross-origin",
+    }
+
+    for header, value in security_headers.items():
+        response.headers[header] = value
+    return response
 
 
 if __name__ == '__main__':
